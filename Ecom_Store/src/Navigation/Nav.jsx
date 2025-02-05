e9513a163e368d268364b4aa8651e0cfb1f8937a
Elitesun <elitesun.dev@gmail.com>
2025-02-05 19:27:10 +0000

    add react-icons dependency and create initial component files

---
+++ Ecom_Store/src/Navigation/Nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {FiHeart} from 'react-icons/fi'
+import {AiPOutlineShoppingCart , AiOutlineUserAdd} from 'react-icons/ai'
+
+const Nav = () => {
+  return (
+    <nav>
+        <div className="nav-container">
+            <input type="text" placeholder="Search Products"
+            className="search-input"/>
+        </div>
+
+
+
+
+    </nav>
+  )
+}
+
+export default Nav
