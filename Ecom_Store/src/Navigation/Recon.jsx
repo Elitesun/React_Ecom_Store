a4cdff62fb34585101a2266b8a11c10f369c397b
Elitesun <elitesun.dev@gmail.com>
2025-02-10 08:43:59 +0000

    feat: enhance styling in Recon component for improved layout and button design

--- Ecom_Store/src/Navigation/Recon.jsx
+++ Ecom_Store/src/Navigation/Recon.jsx
@@ -3,18 +3,17 @@ import React from 'react'
 const Recon = () => {
 return (
 
-<div className=" px-8 ml-[15%]">
-  <h2 className='text-2xl mb-[10px] mt-[10px]'>Recommended</h2>
-  <div className='flex gap-[20px]'>
-    <button>View All</button>
-    <button>Adidas</button>
-    <button>Nike</button>
-    <button>Puma</button>
-  </div>
-</div>
-
-
+<div className=" px-8 ml-[16%]">
+  <h2 className='text-xl font-bold  mb-[10px] mt-[10px] '>Recommended</h2>
+  <div className='flex gap-[10px] font-light '>
+    <button className='border-[0.6px] border-solid border-[#323232] p-1 px-2 rounded-[5px] text-[#323232] cursor-pointer hover:bg-blue-600'>View All</button>
+    <button className='border-[0.6px] border-solid border-[#323232] p-1 px-2 rounded-[5px] text-[#323232] cursor-pointer hover:border-l-blue-600'>Adidas</button>
+    <button className='border-[0.6px] border-solid border-[#323232] p-1 px-2 rounded-[5px] text-[#323232] cursor-pointer hover:border-l-blue-600'>Nike</button>
+    <button className='border-[0.6px] border-solid border-[#323232] p-1 px-2 rounded-[5px] text-[#323232] cursor-pointer hover:border-l-blue-600'>Puma</button>
+  </div> 
+    </div>  
 )
 }
 
+ 
 export default Recon
